1d397741960b35646f770ac906456be16518b43f
Stephanie Wilkinson <what.happens@gmail.com>
2016-12-12 17:06:12 -0800

    first commit

---
+++ ex2.rb
@@ -0,0 +1,27 @@
+module Advent
+  @instruction_array = "R1, R1, R3, R1, R1, L2, R5, L2, R5, R1, R4, L2, R3, L3, R4, L5, R4, R4, R1, L5, L4, R5, R3, L1, R4, R3, L2, L1, R3, L4, R3, L2, R5, R190, R3, R5, L5, L1, R54, L3, L4, L1, R4, R1, R3, L1, L1, R2, L2, R2, R5, L3, R4, R76, L3, R4, R191, R5, R5, L5, L4, L5, L3, R1, R3, R2, L2, L2, L4, L5, L4, R5, R4, R4, R2, R3, R4, L3, L2, R5, R3, L2, L1, R2, L3, R2, L1, L1, R1, L3, R5, L5, L1, L2, R5, R3, L3, R3, R5, R2, R5, R5, L5, L5, R2, L3, L5, L2, L1, R2, R2, L2, R2, L3, L2, R3, L5, R4, L4, L5, R3, L4, R1, R3, R2, R4, L2, L3, R2, L5, R5, R4, L2, R4, L1, L3, L1, L3, R1, R2, R1, L5, R5, R3, L3, L3, L2, R4, R2, L5, L1, L1, L5, L4, L1, L1, R1".split ',' ' '
+
+  @x = 0
+  @y = 0
+  @dir = 360
+
+  @instruction_array.each { |instruction|
+    instruction[0] == "R" ? @dir += 90 : @dir -= 90
+
+    @distance = instruction[1..3].to_i
+
+    @dir %= 360
+
+    if (@dir == 0)
+      @y += @distance
+    elsif (@dir == 270)
+      @x -= @distance
+    elsif (@dir == 180)
+      @y -= @distance
+    elsif (@dir == 90)
+      @x += @distance
+    end
+  }
+
+  puts "final coords: #{@x}, #{@y}"
+end
